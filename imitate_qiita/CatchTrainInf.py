f425f31a8ebdd4b90e5e52231e4baee8ff2d463b
yasuda.hirotake <yasuda.hirotake@tis.co.jp>
2018-08-23 13:22:52 +0900

    インデントを修正

--- imitate_qiita/CatchTrainInf.py
+++ imitate_qiita/CatchTrainInf.py
@@ -1,17 +1,20 @@
-# coding:utf-8
-# 東京メトロのAPIで運行情報をjson形式で取得する
+# -*- coding:utf-8 -*-
+
+""" 東京メトロのAPIで運行情報をjson形式で取得する """
 
 import urllib.parse
 import urllib.request
 
-def trainInf():
+
+def train_info():
 
     url = 'https://api.tokyometroapp.jp/api/v2/datapoints?'
     params = urllib.parse.urlencode(
-            {'rdf:type':'odpt:TrainInformation',
-             'acl:consumerKey':'fd7c104410f76dbd3da3af405da31887fbc82cf695aa0a4bd168180d9f990c6e',
+            {'rdf:type': 'odpt:TrainInformation',
+             'acl:consumerKey': 'fd7c104410f76dbd3da3af405da31887fbc82cf695aa0a4bd168180d9f990c6e',
             }
     )
 
     response = urllib.request.urlopen(url + params)
     return response.read()
+
--- imitate_qiita/Train_Info_Top.py
+++ imitate_qiita/Train_Info_Top.py
@@ -1,8 +1,8 @@
-#coding:utf-8
+# -*- coding:utf-8 -*-
 
 import json
 from trainname import trainname
-from CatchTrainInf import trainInf
+from CatchTrainInf import train_info
 #####
 # sslでエラーが出る。証明書の問題？ セキュリティー上あんま良くないと思う
 import ssl
@@ -10,18 +10,17 @@ ssl._create_default_https_context = ssl._create_unverified_context
 #####
 
 
-
-def TrainInfoTop(TARGET):
+def train_info_top(target):
     # 運行情報を取得
     # jsonをstrに変換して格納
-    infData = json.loads(trainInf())
+    infdata = json.loads(train_info())
 
-    for inf in infData:
+    for inf in infdata:
 
-        if inf["odpt:railway"] == trainname(TARGET):
+        if inf["odpt:railway"] == trainname(target):
 
             # 生データ（デバッグの時とかに見る）
-            # print(inf)
+            print(inf)
 
             # 情報公開日時のフォーマット処理
             date = inf["dc:date"]
@@ -29,14 +28,16 @@ def TrainInfoTop(TARGET):
             date[1] = date[1].split("+")
             date = date[0] + ";" + date[1][0]
 
-            return {\
-                "line":trainname(inf["odpt:railway"]),\
-                "presentationTime":date,\
-                "inf":inf["odpt:trainInformationText"],\
-                "normal_operation":"平常" in inf["odpt:trainInformationText"]\
+            return {
+                "line": trainname(inf["odpt:railway"]),
+                "presentationTime": date,
+                "inf": inf["odpt:trainInformationText"],
+                "normal_operation": "平常" in inf["odpt:trainInformationText"]
                 }
 
 
 # デバッグ用
 if __name__ == '__main__':
-    print(TrainInfoTop("丸ノ内線"))
+
+    print(train_info_top("丸ノ内線"))
+
